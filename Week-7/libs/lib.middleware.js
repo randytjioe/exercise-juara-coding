cf6433e2758f8f683b288c69f7693e16c6da86c9
randytjioe <randytjioe95@gmail.com>
2024-04-22 16:14:21 +0700

    fix validate

--- Week-7/libs/lib.middleware.js
+++ Week-7/libs/lib.middleware.js
@@ -7,42 +7,41 @@ const IsAuthenticated = (req, res, next) => {
   const token = req.headers["authorization"];
 
   if (!token) {
-    Logging.log("error", "Token is required for authentication")
-    return res.status(401).send({detail: "Token is required for authentication"})
+    Logging.log("error", "Token is required for authentication");
+    return res
+      .status(401)
+      .send({ detail: "Token is required for authentication" });
   }
 
   try {
     const decode = jwt.verify(token, process.env.TOKEN_KEY);
     req.user = decode;
   } catch (error) {
-    Logging.log("error", "Invalid token")
-    return res.status(401).send({ detail: "Invalid token" })
-  } 
+    Logging.log("error", "Invalid token");
+    return res.status(401).send({ detail: "Invalid token" });
+  }
 
   return next();
-}
+};
 
 const Validate = (validations) => {
-  return async (err, req, res, next) => {
-
+  return async (req, res, next) => {
     for (let validation of validations) {
-      await validation.run(req);
+      const result = await validation.run(req);
       // if (result.errors.length) break;
     }
-    
-    const errors = validationResult(req);
-    const errs =  _.chain(errors.errors).groupBy("path").value()
 
-    console.log("test", errors.isEmpty())
+    const errors = validationResult(req);
     if (errors.isEmpty()) {
       req.cleanedData = matchedData(req);
       return next();
     }
-    res.status(400).json(errs);
+
+    res.status(400).json({ errors: errors.array() });
   };
-}
+};
 
 module.exports = {
   IsAuthenticated,
-  Validate
-}
+  Validate,
+};
